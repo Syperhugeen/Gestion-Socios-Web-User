7db44df8c3ddaa815a720220e05f83d2512a5d80
Mauricio Costanzo <ventas@worldmaster.com.uy>
2020-08-25 20:30:52 -0300

    ok

--- resources/views/paginas/Estructura_partes/Header/header_comun.blade.php
+++ resources/views/paginas/Estructura_partes/Header/header_comun.blade.php
@@ -5,9 +5,9 @@
           <div class="row align-items-center justify-content-between ">
             
             <div class="col-6 col-lg-2">
-             <a href="{{route('get_home')}}" class="text-left titulos-class" :class="getClassColorElement">
-              <i class="fas fa-code"></i>
-               
+             <a href="{{route('get_home')}}" class="d-block w-100 p-3 p-lg-0">
+               <img v-if="$root.scrolled > 0" src="{{url()}}/imagenes/Empresa/logo-rectangular-easysocio-color.png" class="img-fluid">
+               <img v-else src="{{url()}}/imagenes/Empresa/logo-rectangular-easysocio-blanco.png" class="img-fluid">
 
              </a>
             </div> 
@@ -17,34 +17,12 @@
             
               <nav v-if="mostrar_opciones_del_menu" class="col-lg-8 d-flex flex-column align-items-end" role="navigation" :class="getClassMostrarMenuMovil">
                 <ul :class="getClassNavUlEnMovil" class="d-flex flex-column align-items-start flex-lg-row align-items-lg-center m-0 px-lg-3">     
-                  <div v-if="$root.mostrar_para_celuar"  v-on:click="set_mostrar_menu_movil" class="p-2 w-100 text-right titulos-class"><i class="fas fa-times"></i>
+                  <div v-if="$root.mostrar_para_celuar"  v-on:click="set_mostrar_menu_movil" class="p-5 w-100 text-right "><i class="fas fa-times"></i>
                   </div>
-                  
-                    <li :class="getClassUlLI"><a :class="getClassItemsNav" href="{{route('get_home')}}" >Inicio</a></li>
-
-                    {{--  S u b   m e n ú  --}}
-                   {{--  <menu-primer-triada name_padre="Desarrollo web" url_padre="{{route('get_home')}}" >
-
-                     
-                     <template slot="opciones" >
-
-                       
-
-                       <li class="header-li-primer-tria">
-                         <a href="{{route('get_pagina_de_web_para_pymes')}}">Para PyMes</a>
-                       </li>
-                        <li class="header-li-primer-tria">
-                          <a href="{{route('get_pagina_de_web_profesionales')}}">Para profesionales</a>
-                        </li>
-
-                     </template>
-
-                    </menu-primer-triada> --}}
-                  
-                  {{-- <li class="header-nav-ul-li"><a :class="getClassItemsNav" href="#about-section" class="nav-link">Sobre Mauricio Costanzo</a></li>
-                  <li class="header-nav-ul-li"><a :class="getClassItemsNav" href="#services-section" class="nav-link">Servicios</a></li>       --}}     
-                  <li :class="getClassUlLI"><a :class="getClassItemsNav" href="{{route('get_pagina_noticias_listado')}}">Blog</a></li>
-                  <li :class="getClassUlLI"><a :class="getClassItemsNav" href="{{route('get_pagina_contacto','-')}}" >Contacto</a>
+                  <li class="header-nav-ul-li"><a :class="getClassItemsNav" href="{{route('get_home')}}" class="nav-link">Inicio</a></li>
+                         
+                  <li class="header-nav-ul-li"><a :class="getClassItemsNav" href="{{route('get_pagina_noticias_listado')}}" class="nav-link">Blog</a></li>
+                  <li class="header-nav-ul-li"><a :class="getClassItemsNav" href="{{route('get_pagina_contacto','-')}}" class="nav-link">Contacto</a>
                   </li>
                   
                 </ul>
@@ -58,4 +36,5 @@
         </div>      
   </header>
 </header-nav>
+
 
