45d8be0e596c46ac63f3eeb068709b95523f0c08
Mauricio <ventas@worldmaster.com.uy>
2021-01-30 21:21:58 -0300

    Cree planes api

--- app/Http/Controllers/Publicas/Home_Public_Controller.php
+++ app/Http/Controllers/Publicas/Home_Public_Controller.php
@@ -3,48 +3,40 @@ namespace App\Http\Controllers\Publicas;
 
 use App\Http\Controllers\Controller;
 use App\Repositorios\EmpresaRepo;
-use Illuminate\Http\Request;
 use App\Repositorios\NoticiasRepo;
 use App\Repositorios\PortadaDePaginaRepo;
-use Illuminate\Support\Facades\Cache;
 use App\Servicios\ServiciosPlanes;
-
-
-
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class Home_Public_Controller extends Controller
 {
-   
-    protected $EmpresaRepo;
-    protected $NoticiasRepo;  
-    protected $PortadaDePaginaRepo;
-    protected $ServiciosPlanes;
-  
 
-    public function __construct(EmpresaRepo         $EmpresaRepo, 
-                                NoticiasRepo        $NoticiasRepo, 
-                                PortadaDePaginaRepo $PortadaDePaginaRepo, 
-                                ServiciosPlanes     $ServiciosPlanes
-                                )
-    {        
-        $this->EmpresaRepo            = $EmpresaRepo;
-        $this->NoticiasRepo           = $NoticiasRepo;
-        $this->PortadaDePaginaRepo    = $PortadaDePaginaRepo;
-        $this->ServiciosPlanes        = $ServiciosPlanes;
+    protected $EmpresaRepo;
+    protected $NoticiasRepo;
+    protected $PortadaDePaginaRepo;
+
+    public function __construct(EmpresaRepo $EmpresaRepo,
+        NoticiasRepo $NoticiasRepo,
+        PortadaDePaginaRepo $PortadaDePaginaRepo
+    ) {
+        $this->EmpresaRepo = $EmpresaRepo;
+        $this->NoticiasRepo = $NoticiasRepo;
+        $this->PortadaDePaginaRepo = $PortadaDePaginaRepo;
+
     }
 
     public function get_home(Request $Request)
-    {                  
-        $Empresa        = $this->EmpresaRepo->getEmpresaDatos();         
-        $blogs          = $this->NoticiasRepo->getUltimosBlogs();    
-        $Portada        = Cache::remember('PortadaHome', 2000, function(){
-                          return $this->PortadaDePaginaRepo->getFirstEntidadSegunAtributo('name','home');
-                          });  
+    {
+        $Empresa = $this->EmpresaRepo->getEmpresaDatos();
+        $blogs = $this->NoticiasRepo->getUltimosBlogs();
+        $Portada = Cache::remember('PortadaHome', 2000, function () {
+            return $this->PortadaDePaginaRepo->getFirstEntidadSegunAtributo('name', 'home');
+        });
 
-        $Planes         = $this->ServiciosPlanes->getPlanes();
+        $Planes = ServiciosPlanes::getPlanes();
 
-        return view('paginas.home.home', compact('Empresa','blogs','Portada','Planes'));
+        return view('paginas.home.home', compact('Empresa', 'blogs', 'Portada', 'Planes'));
     }
-
 
 }
--- app/Servicios/ServiciosPlanes.php
+++ app/Servicios/ServiciosPlanes.php
@@ -24,7 +24,6 @@ class ServiciosPlanes
 
         });
 
-        dd($Planes);
         return $Planes;
 
     }
