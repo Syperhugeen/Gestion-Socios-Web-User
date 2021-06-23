0e22eea75e906b47453fd86d749464da6bf61681
Mauricio Costanzo <ventas@worldmaster.com.uy>
2021-06-23 16:17:03 -0300

    fg

--- app/Entidades/Imagen.php
+++ app/Entidades/Imagen.php
@@ -2,82 +2,74 @@
 
 namespace App\Entidades;
 
-use Illuminate\Database\Eloquent\Model;
 use App\Helpers\HelpersGenerales;
-
-
-
+use Illuminate\Database\Eloquent\Model;
 
 class Imagen extends Model
 {
 
-    protected $table    ='imagenes';
+    protected $table    = 'imagenes';
     protected $fillable = ['name'];
-    protected $appends  = ['url_img','url_img_chica','path_url_img','path_url_img_chica','url_img_vertical','url_img_vertical_chica'];
+    protected $appends  = ['url_img', 'url_img_chica', 'path_url_img', 'path_url_img_chica', 'url_img_vertical', 'url_img_vertical_chica'];
 
-    
     public function getUrlImgAttribute()
-    {        
-        return url().'/imagenes/'. $this->path . HelpersGenerales::helper_convertir_cadena_para_url($this->name) . '-'.$this->id . '.jpg';
+    {
+        return 'https://gestionsocios.com.uy/imagenes/' . $this->path . HelpersGenerales::helper_convertir_cadena_para_url($this->name) . '-' . $this->id . '.jpg';
     }
 
     public function getUrlImgChicaAttribute()
-    {        
-        return url().'/imagenes/'. $this->path . HelpersGenerales::helper_convertir_cadena_para_url($this->name) . '-'.$this->id . '-chica.jpg';
+    {
+        return 'https://gestionsocios.com.uy/imagenes/' . $this->path . HelpersGenerales::helper_convertir_cadena_para_url($this->name) . '-' . $this->id . '-chica.jpg';
     }
 
     public function getUrlImgVerticalAttribute()
-    {        
-        return url().'/imagenes/'. $this->path . HelpersGenerales::helper_convertir_cadena_para_url($this->name) . '-'.$this->id . '-vertical.jpg';
+    {
+        return 'https://gestionsocios.com.uy/imagenes/' . $this->path . HelpersGenerales::helper_convertir_cadena_para_url($this->name) . '-' . $this->id . '-vertical.jpg';
     }
 
     public function getUrlImgVerticalChicaAttribute()
-    {        
-        return url().'/imagenes/'. $this->path . HelpersGenerales::helper_convertir_cadena_para_url($this->name) . '-'.$this->id . '-vertical-chica.jpg';
+    {
+        return 'https://gestionsocios.com.uy/imagenes/' . $this->path . HelpersGenerales::helper_convertir_cadena_para_url($this->name) . '-' . $this->id . '-vertical-chica.jpg';
     }
 
     public function getPathUrlImgAttribute()
-    {        
-        return public_path().'/imagenes/'. $this->path . HelpersGenerales::helper_convertir_cadena_para_url($this->name) . '-'.$this->id . '.jpg';
+    {
+        return public_path() . '/imagenes/' . $this->path . HelpersGenerales::helper_convertir_cadena_para_url($this->name) . '-' . $this->id . '.jpg';
     }
 
     public function getPathUrlImgChicaAttribute()
-    {        
-        return public_path().'/imagenes/'. $this->path . HelpersGenerales::helper_convertir_cadena_para_url($this->name) . '-'.$this->id . '-chica.jpg';
+    {
+        return public_path() . '/imagenes/' . $this->path . HelpersGenerales::helper_convertir_cadena_para_url($this->name) . '-' . $this->id . '-chica.jpg';
     }
 
     public function getPathUrlImgVerticalAttribute()
-    {        
-        return public_path().'/imagenes/'. $this->path . HelpersGenerales::helper_convertir_cadena_para_url($this->name) . '-'.$this->id . '-vertical.jpg';
+    {
+        return public_path() . '/imagenes/' . $this->path . HelpersGenerales::helper_convertir_cadena_para_url($this->name) . '-' . $this->id . '-vertical.jpg';
     }
 
     public function getPathUrlImgVerticalChicaAttribute()
     {
-        
-        return public_path().'/imagenes/'. $this->path . HelpersGenerales::helper_convertir_cadena_para_url($this->name) . '-'.$this->id . '-vertical-chica.jpg';
-    }   
 
+        return public_path() . '/imagenes/' . $this->path . HelpersGenerales::helper_convertir_cadena_para_url($this->name) . '-' . $this->id . '-vertical-chica.jpg';
+    }
 
     public function getEsImagenPrincipalAttribute()
     {
-        if($this->foto_principal == 'si')
-        {
+        if ($this->foto_principal == 'si') {
             return true;
-        }
-        else
-        {
+        } else {
             return false;
         }
     }
 
     public function getEliminarRouteAttribute()
     {
-        return route('borrar_esta_imagen',$this->id);
+        return route('borrar_esta_imagen', $this->id);
     }
 
     public function getCambiarAPricnipalRouteAttribute()
     {
-        return route('establecer_como_principal',$this->id);
+        return route('establecer_como_principal', $this->id);
     }
-    
+
 }
