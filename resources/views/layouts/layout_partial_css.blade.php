b9776b0f373216b995a0cee39e93598a396b2d78
Mauricio Costanzo <ventas@worldmaster.com.uy>
2021-07-09 15:34:16 -0300

    gh

--- resources/views/layouts/layout_partial_css.blade.php
+++ resources/views/layouts/layout_partial_css.blade.php
@@ -2,14 +2,14 @@
 
 
 
-<link rel="preconnect"href="https://unpkg.com/"crossorigin />
-<link rel="dns-prefetch"href="https://unpkg.com/" />
+<link rel="preconnect" href="https://unpkg.com/"crossorigin />
+<link rel="dns-prefetch" href="https://unpkg.com/" />
 
-<link rel="preconnect"href="https://use.fontawesome.com/"crossorigin />
-<link rel="dns-prefetch"href="https://use.fontawesome.com/" />
+<link rel="preconnect" href="https://use.fontawesome.com/"crossorigin />
+<link rel="dns-prefetch" href="https://use.fontawesome.com/" />
 
-<link rel="preconnect"href="https://www.googletagmanager.com/"crossorigin />
-<link rel="dns-prefetch"href="https://www.googletagmanager.com/" />
+<link rel="preconnect" href="https://www.googletagmanager.com/"crossorigin />
+<link rel="dns-prefetch" href="https://www.googletagmanager.com/" />
 
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
