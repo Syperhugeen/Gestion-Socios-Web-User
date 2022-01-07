9cde0a26fc3ed50d663520ca09e1ce79ac462109
Mauricio <ventas@worldmaster.com.uy>
2022-01-07 09:22:03 -0300

    sd

--- app/Http/Rutas/Noticias/Rutas_Noticias_Publicas.php
+++ app/Http/Rutas/Noticias/Rutas_Noticias_Publicas.php
@@ -9,7 +9,10 @@ Route::get('/blog', [
 Route::get('/blog/{name}/{id}', [
     'uses' => 'Publicas\Paginas_Controller@get_pagina_noticia_individual',
     'as'   => 'get_pagina_noticia_individual']
-);
+)->where([
+    'id'   => '[0-9]+',
+    'name' => '[ 0-9A-Za-zñÑáéíóúÁÉÍÓÚ +-.,]+',
+]);
 
 Route::get('/blog-newsletter-{id}-{email}', [
     'uses' => 'Publicas\Paginas_Controller@get_pagina_newsletter_noticias_listado',
