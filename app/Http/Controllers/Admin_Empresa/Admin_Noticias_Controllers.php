ee9ff578d1aa07651713d86991039f19f176e8ac
Mauricio <ventas@worldmaster.com.uy>
2022-01-13 20:18:18 -0300

    fg

--- app/Http/Controllers/Admin_Empresa/Admin_Noticias_Controllers.php
+++ app/Http/Controllers/Admin_Empresa/Admin_Noticias_Controllers.php
@@ -128,7 +128,7 @@ class Admin_Noticias_Controllers extends Controller implements entidadCrudContro
                 }
                 catch (\Exception$e)
                 {
-                    if ($e->getMessage() == 'Swift_RfcComplianceException')
+                    if ($e == 'Swift_RfcComplianceException')
                     {
                         $UserNewsletterRepo->setAtributoEspecifico($UserNewsletter, 'se_puede_enviar', 'no');
                     }
