7047a1d5df1205f37138d50251509b819a12063c
Mauricio Costanzo <mauricio@worldmaster.com.uy>
2024-11-21 17:50:29 -0300

    Add Meta Pixel Code for Facebook tracking in layout_principal.blade.php

--- resources/views/layouts/credo/layout_pricnipal.blade.php
+++ resources/views/layouts/credo/layout_pricnipal.blade.php
@@ -14,6 +14,24 @@
     @yield('pixcel-facebook')
     @yield('favicon')
 
+    <!-- Meta Pixel Code -->
+    <script>
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '1289810099135333');
+    fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none"
+    src="https://www.facebook.com/tr?id=1289810099135333&ev=PageView&noscript=1"
+    /></noscript>
+    <!-- End Meta Pixel Code -->
+
     <script type="application/ld+json">{"@context":"https:\/\/schema.org","@type":"WebApplication","name":"Easysocio","applicationCategory":"Gestión","aggregateRating":{"@type":"AggregateRating","ratingValue":4.9,"ratingCount":11863}}</script>
 
     <meta name="facebook-domain-verification" content="20ezzwn14s6bw0rp4q3711awjo32zc" />
