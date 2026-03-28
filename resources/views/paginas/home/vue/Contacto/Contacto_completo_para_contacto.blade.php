83acb520a56c34e759bb11810ff453b0a46cfa1c
Mauricio Costanzo <mauricio@worldmaster.com.uy>
2026-03-28 13:14:49 -0300

    Remove contact form section and related input fields from Contacto_completo_para_contacto view

--- resources/views/paginas/home/vue/Contacto/Contacto_completo_para_contacto.blade.php
+++ resources/views/paginas/home/vue/Contacto/Contacto_completo_para_contacto.blade.php
@@ -26,41 +26,12 @@
           Click aquí para enviar mensaje <i class="fab fa-whatsapp"></i>
 
           </a>  </li>
-          <li class="mb-3">  Rellená el formulario de aquí abajo y te responderemos muy rápido por email  <i class="fas fa-hand-point-down"></i> </li>
-    </ol>
+          </ol>
 
 
 
 
-    <div class="row mx-0 w-100 mb-5 ">
-      <h3 class="col-12 text-bold mb-4">1 - Explicá lo que necesitás.</h3>
-      <div class="col-12">
-        <textarea v-model="data_mensaje.mensaje" class="border-primary" cols="30" rows="4" :class="classImput" placeholder="Explica brevemente lo que necesitás"></textarea>
-      </div>
-    </div>
-
-
-    <div class="row mx-0 w-100 mb-5">
-
-      <h3 class="col-12 text-bold  mb-4">2 - Dejá tus datos.</h3>
-
-      <div class="col-12 col-lg-6 mb-2">
-        <input v-model="data_mensaje.name" type="text" :class="classImput" placeholder="Tu nombre">
-      </div>
-
-       <div class="col-12 col-lg-6 mb-2">
-         <input v-model="data_mensaje.nombre_empresa" type="text" :class="classImput" placeholder="Tu empresa">
-      </div>
-
-      <div class="col-12 col-lg-6 mb-2">
-        <input v-model="data_mensaje.email" type="email" :class="classImput" placeholder="Tu email">
-      </div>
-
-      <div class="col-12 col-lg-6 mb-2">
-        <input v-model="data_mensaje.celular" type="number" :class="classImput" placeholder="Un celular válido">
-      </div>
-
-    </div>
+   
 
 
 
@@ -73,27 +44,6 @@
 
 
 
-
-
-    <div class="row mx-0 mt-4">
-
-      <div class="col-md-6" v-if="errores" >
-        <div :class="classTextColor" v-for="error in errores">@{{error}}</div>
-      </div>
-
-      <div class="col-md-12">
-        <div v-if="cargando" class="flex-column align-items-center">
-          <div class="cssload-tube-tunnel" :class="classCargadorColor"></div>
-        </div>
-        <div v-else v-on:click="enviarMensaje" :class="classBoton" value="Enviar mensaje">
-          Enviar solicitud ahora
-        </div>
-      </div>
-      <div>
-
-      </div>
-
-    </div>
 
 
 
