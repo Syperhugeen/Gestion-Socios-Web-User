4718a51f3f0653b628e90085b0aaf82ad5b9972f
Mauricio Costanzo <mauricio@worldmaster.com.uy>
2026-03-28 13:21:15 -0300

    Remove post_contacto_form route from Rutas_Formularios_Publicas.php

--- app/Http/Rutas/Formularios/Rutas_Formularios_Publicas.php
+++ app/Http/Rutas/Formularios/Rutas_Formularios_Publicas.php
@@ -1,11 +1,6 @@
 <?php
 
-//Post Form Contacto
-Route::post('post_contacto_form',
-    [
-        'uses' => 'Publicas\Envio_Formularios_Controller@post_contacto_form',
-        'as'   => 'post_contacto_form',
-    ]);
+
 
 Route::post('post_nuevo_newsletter',
     [
